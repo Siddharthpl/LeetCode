a59051bbfc9a241b83c165d6e445332155fec73d
Siddharthpl <145639697+Siddharthpl@users.noreply.github.com>
2024-08-05 15:44:35 +0530

    Time: 2 ms (70.99%), Space: 8.5 MB (50.66%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    string solve(string num1, string num2,int p1,int p2,int carry = {0}){
+    void solve(string& num1, string& num2,int p1,int p2,int carry, string& ans){
         if(p1<0 && p2<0){
             if(carry!=0){
-                return string(1,carry+'0');
+                ans.push_back(carry+'0');
             }
-            return "";
+            return ;
         }
         int n1 = (p1>=0 ? num1[p1] :'0') - '0';
         int n2 = (p2>=0 ? num2[p2] :'0') - '0';
         int sum = n1 + n2 + carry;
         int digit = sum%10;
         carry = sum/10;
-        string ans = "";
+        
         ans.push_back(digit + '0');
 
-        ans +=solve(num1,num2,p1-1,p2-1,carry);
+        solve(num1,num2,p1-1,p2-1,carry,ans);
 
-        return ans;
+        return;
       
         
     }
 
     string addStrings(string num1, string num2) {
-        
-       
-        
-        string ans = solve(num1,num2,num1.length()-1,num2.length()-1);
+        string ans = "";
+        solve(num1,num2,num1.length()-1,num2.length()-1,0,ans);
         reverse(ans.begin(),ans.end());
         return ans;
 
