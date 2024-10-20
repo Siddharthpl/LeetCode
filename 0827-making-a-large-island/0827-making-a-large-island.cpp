d9fc89c23914ab0a82204deadc8ebeee332a3aa2
Siddharth <145639697+Siddharthpl@users.noreply.github.com>
2024-10-20 11:36:15 +0530

    Time: 302 ms (53.96%), Space: 172.8 MB (23.29%) - LeetHub

---
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// User function Template for C++
+class DisjointSet {
+
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int largestIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>> visited(n,vector<int>(n,0));
+        DisjointSet ds(n*n);
+
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,1,0,-1};
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if( grid[i][j]==1){
+                    
+                    for(int k=0;k<4;k++){
+                        int nr = i + dr[k];
+                        int nc = j + dc[k];
+                        if(nr>=0 && nc>=0 && nr<n && nc<n && grid[nr][nc]==1){
+                            int ci = i*n + j;
+                            int ni = nr*n + nc;
+                            ds.unionBySize(ci,ni);
+                        }
+                    }
+                }
+            }
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0){
+                    unordered_set<int> set;
+                    for(int k=0;k<4;k++){
+                        int nr = i + dr[k];
+                        int nc = j + dc[k];
+                        if(nr>=0 && nc>=0 && nr<n && nc<n && grid[nr][nc]==1){
+                            int ni = nr*n + nc;
+                            int up = ds.findUPar(ni);
+                            set.insert(up);
+                        }
+                    }
+                    int sizeTotal = 0;
+                    for (auto it : set) {
+                        sizeTotal += ds.size[it];
+                    }
+                    ans = max(ans, sizeTotal + 1);
+                }
+            }
+        }
+        for (int cellNo = 0; cellNo < n * n; cellNo++) {
+            ans = max(ans, ds.size[ds.findUPar(cellNo)]);
+        }
+        return ans;
+    }
+};
